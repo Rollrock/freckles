767e193d52e0c60ee4f14434454a148b37609cad
zhuang chaoxiao <zhuangchaoxiao@163.com>
2014-11-23 08:29:48 +0800

    初始化

---
+++ Freckles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Freckles
+//
+//  Created by zhuang chaoxiao on 14-11-21.
+//  Copyright (c) 2014年 zhuang chaoxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Freckles/ViewController.m
@@ -0,0 +1,46 @@
+//
+//  ViewController.m
+//  Freckles
+//
+//  Created by zhuang chaoxiao on 14-11-21.
+//  Copyright (c) 2014年 zhuang chaoxiao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *firstBtn;
+@property (weak, nonatomic) IBOutlet UIButton *secondBtn;
+- (IBAction)firstBtnClicked;
+- (IBAction)secondBtnClicked;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+-(void)layoutSubView
+{
+    
+}
+
+- (IBAction)firstBtnClicked {
+}
+
+- (IBAction)secondBtnClicked {
+}
+
+- (IBAction)secondBtnClicked:(id)sender {
+}
+@end
